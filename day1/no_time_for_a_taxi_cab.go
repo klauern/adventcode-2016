eb4d75776afa264c6643949d24cf8e94dcbebff6
Nick Klauer <klauer@gmail.com>
2016-12-02 20:37:10 -0600

    add calculate direciton

--- day1/no_time_for_a_taxi_cab.go
+++ day1/no_time_for_a_taxi_cab.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/klauern/adventcode-2016/helpers"
-import "strings"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/klauern/adventcode-2016/helpers"
+)
 
 // Position represents a position on a grid, all relative on an X/Y plane from point 0,0
 type Position struct {
 	X,
 	Y int
-	DirectionFacing string
+	DirectionFacing rune
 }
 
 func main() {
@@ -16,12 +20,36 @@ func main() {
 
 	current := &Position{}
 	for _, next := range inputs {
-		current.CalculatePosition(next)
+		current.CalculateDirection(next)
 	}
 }
 
-// CalculatePosition will calculate the next Position of an entry
+// CalculateDirection will calculate the next Position of an entry
 // given the movement string.
-func (p Position) CalculatePosition(next string) Position {
-	return p
+func (p Position) CalculateDirection(next string) rune {
+	turn := next[0]
+	fmt.Printf("Turning %x\n", turn)
+	switch p.DirectionFacing {
+	case 'N':
+		if turn == 'L' {
+			return 'W'
+		}
+		return 'E'
+	case 'E':
+		if turn == 'L' {
+			return 'N'
+		}
+		return 'S'
+	case 'W':
+		if turn == 'L' {
+			return 'S'
+		}
+		return 'N'
+	case 'S':
+		if turn == 'L' {
+			return 'E'
+		}
+		return 'W'
+	}
+	return 'G'
 }
