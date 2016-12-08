3ff7409dcc0a336614aac53df26f6013972020e9
Nick Klauer <klauer@gmail.com>
2016-12-08 10:40:24 -0600

    formatting and such

--- day4/security_through_obscurity.go
+++ day4/security_through_obscurity.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Room struct {
-	fields   struc
-	sector   int
-	checksum string
+	maxRuneField map[string]rune
+	sector       int
+	checksum     string
 }
 
 func (r *Room) CalcChecksum() {
@@ -19,9 +19,12 @@ func NewRoom(line string) *Room {
 	fields := strings.Split(line, "-")
 	room := &Room{}
 	for _, v := range fields[:len(fields)-1] {
+        maxRuneField[v] =
 	}
 	return room
 }
+
+func calcMax
 
 func splitSectorChecksum(s string) (int, string) {
 	secksum := strings.Split(s, "[")
