e38156a02d52f0bebf6a3c57cbefce9a9b28c679
Nick Klauer <klauer@gmail.com>
2016-12-10 16:56:15 -0600

    add test for least common

--- day6/signals_and_noise_test.go
+++ day6/signals_and_noise_test.go
@@ -118,7 +118,7 @@ func TestNewCode(t *testing.T) {
 	}
 }
 
-func TestDecodeString(t *testing.T) {
+func TestDecodeMostCommonString(t *testing.T) {
 	type args struct {
 		s string
 	}
@@ -153,6 +153,132 @@ enarar`,
 	for _, tt := range tests {
 		if got := DecodeMostCommonString(tt.args.s); got != tt.want {
 			t.Errorf("%q. DecodeString() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestDecodeLeastCommonString(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"thing",
+			args{
+				`eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar`,
+			},
+			"advent",
+		},
+	}
+	for _, tt := range tests {
+		if got := DecodeLeastCommonString(tt.args.s); got != tt.want {
+			t.Errorf("%q. DecodeLeastCommonString() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_code_DecodeLeastCommon(t *testing.T) {
+	tests := []struct {
+		name string
+		c    code
+		want string
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := tt.c.DecodeLeastCommon(); got != tt.want {
+			t.Errorf("%q. code.DecodeLeastCommon() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_mostCommonRune(t *testing.T) {
+	type args struct {
+		runeCount map[rune]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := mostCommonRune(tt.args.runeCount); got != tt.want {
+			t.Errorf("%q. mostCommonRune() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findMostCommonRune(t *testing.T) {
+	type args struct {
+		runes []rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findMostCommonRune(tt.args.runes); got != tt.want {
+			t.Errorf("%q. findMostCommonRune() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_findLeastCommonRune(t *testing.T) {
+	type args struct {
+		runes []rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := findLeastCommonRune(tt.args.runes); got != tt.want {
+			t.Errorf("%q. findLeastCommonRune() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func Test_leastCommonRune(t *testing.T) {
+	type args struct {
+		runeCount map[rune]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		if got := leastCommonRune(tt.args.runeCount); got != tt.want {
+			t.Errorf("%q. leastCommonRune() = %v, want %v", tt.name, got, tt.want)
 		}
 	}
 }
