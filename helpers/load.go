59ac1ec7564c2433a103311f38ebefacc5c602fc
Nick Klauer <klauer@gmail.com>
2016-12-12 19:49:04 -0600

    add scanner loader

--- helpers/load.go
+++ helpers/load.go
@@ -1,7 +1,9 @@
 package helpers
 
 import (
+	"bufio"
 	"io/ioutil"
+	"os"
 )
 
 func MustLoadFile(path string) string {
@@ -10,4 +12,14 @@ func MustLoadFile(path string) string {
 		panic(err)
 	}
 	return string(b)
+}
+
+func MustScanFile(path string) *bufio.Scanner {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	return scanner
 }
