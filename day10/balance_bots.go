ba8e6126450cce2838cbd13bfdac4c667b0a8561
Nick Klauer <klauer@gmail.com>
2016-12-19 17:38:05 -0600

    more stubbing

--- day10/balance_bots.go
+++ day10/balance_bots.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 type valueMove struct {
 	value,
 	bot int
@@ -8,4 +13,35 @@ type valueMove struct {
 type movement struct {
 	high,
 	low int
+}
+
+type value int
+type bot int
+
+type InstructionSet struct {
+	valueList    map[value]bot
+	movementList []movement
+}
+
+func compileInstructions(instructions []string) (*InstructionSet, error) {
+
+	for _, v := range instructions {
+		if strings.HasPrefix(v, "value") {
+
+		}
+	}
+	return nil, nil
+}
+
+func NewValue(val string) (bot, value) {
+	fields := strings.Fields(val)
+	botVal, err := strconv.Atoi(fields[5])
+	if err != nil {
+		panic(err)
+	}
+	valueVal, err := strconv.Atoi(fields[1])
+	if err != nil {
+		panic(err)
+	}
+	return bot(botVal), value(valueVal)
 }
