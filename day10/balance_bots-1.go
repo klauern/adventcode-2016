29ce4f8a1f9f832d254d00042e4de5f46cf71dda
Nick Klauer <klauer@gmail.com>
2016-12-20 07:32:47 -0600

    working on some things
    I have a minor change that results in a whole swath of changes.

--- day10/balance_bots-1.go
+++ day10/balance_bots-1.go
@@ -3,13 +3,30 @@ package main
 import (
 	"strings"
 
+	"strconv"
+
 	"github.com/klauern/adventcode-2016/helpers"
+)
+
+type DestType int
+
+const (
+	bot DestType = iota
+	output
+	neither
 )
 
 type BotState struct {
 	botMap      map[int][]int
 	outputMap   map[int][]int
-	movementMap map[int]int
+	movementMap map[int]*Movement
+}
+
+type Movement struct {
+	highDest int
+	lowDest  int
+	highType DestType
+	lowType  DestType
 }
 
 func (b *BotState) parseLine(line string) {
@@ -22,11 +39,51 @@ func (b *BotState) parseLine(line string) {
 }
 
 func (b *BotState) addValue(line string) {
-
+	fields := strings.Fields(line)
+	value, err := strconv.Atoi(fields[1])
+	if err != nil {
+		panic(err)
+	}
+	botDest, err := strconv.Atoi(fields[5])
+	if err != nil {
+		panic(err)
+	}
+	b.botMap[botDest] = append(b.botMap[botDest], value)
 }
 
 func (b *BotState) addMovement(line string) {
+	fields := strings.Fields(line)
+	botNum, err := strconv.Atoi(fields[1])
+	if err != nil {
+		panic(err)
+	}
+	lowNum, err := strconv.Atoi(fields[6])
+	if err != nil {
+		panic(err)
+	}
+	lowDest := parseDest(fields[5])
+	highNum, err := strconv.Atoi(fields[11])
+	if err != nil {
+		panic(err)
+	}
+	highDest := parseDest(fields[11])
+	movement := &Movement{
+		highDest: highNum,
+		highType: highDest,
+		lowDest:  lowNum,
+		lowType:  lowDest,
+	}
+	b.movementMap[botNum] = movement
+}
 
+func parseDest(val string) DestType {
+	switch val {
+	case "output":
+		return output
+	case "bot":
+		return bot
+	}
+	return neither
 }
 
 func main() {
