bc84a9309edb654d3dc52e7a7976127d222f522e
Nick Klauer <klauer@gmail.com>
2016-12-24 10:00:48 -0600

    attempt all over run

--- day10/balance_bots.go
+++ day10/balance_bots.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"strings"
-
+	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/klauern/adventcode-2016/helpers"
 )
@@ -29,12 +29,13 @@ type Movement struct {
 	lowType  DestType
 }
 
-func (b *BotState) parseLine(line string) {
+func (b *BotState) addLineAssignment(line string) {
 	fields := strings.Fields(line)
 	switch fields[0] {
 	case "bot":
+		b.addMovement(line)
 	case "value":
-
+		b.addValue(line)
 	}
 }
 
@@ -86,9 +87,44 @@ func parseDest(val string) DestType {
 	return neither
 }
 
+func (b *BotState) calcBotMove() {
+	for k, v := range b.botMap {
+		if len(v) == 2 {
+			b.move(k)
+		}
+	}
+}
+
+func (b *BotState) move(botID int) {
+	movement := b.movementMap[botID]
+	high, low := highLow(b.botMap[botID])
+	if movement.lowType == output {
+		b.outputMap[movement.lowDest] = append(b.outputMap[movement.lowDest], low)
+	} else if movement.lowType == bot {
+		b.botMap[movement.lowDest] = append(b.botMap[movement.lowDest], low)
+	}
+	if movement.highType == output {
+		b.outputMap[movement.highDest] = append(b.outputMap[movement.highDest], high)
+	} else if movement.highType == bot {
+		b.botMap[movement.highDest] = append(b.botMap[movement.highDest], high)
+	}
+}
+
+func highLow(vals []int) (int, int) {
+	if vals[0] > vals[1] {
+		return vals[0], vals[1]
+	}
+	return vals[1], vals[0]
+}
+
 func main() {
+	botState := &BotState{botMap: make(map[int][]int, 0), movementMap: make(map[int]*Movement, 0), outputMap: make(map[int][]int, 0)}
 	file := helpers.MustScanFile("input.txt")
 	for file.Scan() {
-
+		botState.addLineAssignment(file.Text())
 	}
+	for k, v := range botState.botMap {
+		fmt.Printf("Bot %v has %v chips\n", k, v)
+	}
+
 }
