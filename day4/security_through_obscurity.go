948da5964c9ac16a83348c3bf6bd08609e6daed9
Nick Klauer <klauer@gmail.com>
2016-12-29 11:08:26 -0600

    add a couple tests and restarts for day4

--- day4/security_through_obscurity.go
+++ day4/security_through_obscurity.go
@@ -1,45 +1,21 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
+import "strings"
 
 type Room struct {
-	maxRuneField map[string]rune
-	sector       int
-	checksum     string
+	sectors  []string
+	checksum string
 }
 
-func (r *Room) CalcChecksum() {
-
+func Checksum(room string) string {
+	strs := strings.Split(room, "[")
+	return strings.TrimRight(strs[1], "]")
 }
 
-func NewRoom(line string) *Room {
-	fields := strings.Split(line, "-")
-	room := &Room{}
-	for _, v := range fields[:len(fields)-1] {
-        maxRuneField[v] =
+func CountString(room string) map[rune]int {
+	charMap := make(map[rune]int, 0)
+	for _, char := range room {
+		charMap[char]++
 	}
-	return room
-}
-
-func calcMax
-
-func splitSectorChecksum(s string) (int, string) {
-	secksum := strings.Split(s, "[")
-	cksum := strings.Trim(secksum[1], "]")
-	room, err := strconv.Atoi(secksum[0])
-	if err != nil {
-		panic(err)
-	}
-	return room, cksum
-}
-
-func (r *Room) RealRoom() bool {
-	return false
-}
-
-func main() {
-
+	return charMap
 }
---
+++ day4/security_through_obscurity_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestChecksum(t *testing.T) {
+	type args struct {
+		room string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"aaaaa-bbb-z-y-x-123[abxyz]",
+			args{"aaaaa-bbb-z-y-x-123[abxyz]"},
+			"abxyz",
+		},
+		{
+			"a-b-c-d-e-f-g-h-987[abcde]",
+			args{"a-b-c-d-e-f-g-h-987[abcde]"},
+			"abcde",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Checksum(tt.args.room); got != tt.want {
+				t.Errorf("Checksum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCountString(t *testing.T) {
+	type args struct {
+		room string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[rune]int
+	}{
+		{
+			"aaaabbbzyx",
+			args{"aaaabbbzyx"},
+			map[rune]int{'a': 4, 'b': 3, 'z': 1, 'y': 1, 'x': 1},
+		},
+		{
+			"abcdefgh",
+			args{"abcdefgh"},
+			map[rune]int{'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1, 'h': 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CountString(tt.args.room); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CountString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
