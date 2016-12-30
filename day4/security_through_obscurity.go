898576752ecc45cec9ffcfd2ce40de0ecafcb678
Nick Klauer <klauer@gmail.com>
2016-12-30 10:12:39 -0600

    add sort functions

--- day4/security_through_obscurity.go
+++ day4/security_through_obscurity.go
@@ -1,11 +1,29 @@
 package main
 
 import "strings"
+import "sort"
 
 type Room struct {
 	sectors  []string
 	checksum string
 }
+
+type RuneMap struct {
+	runeCount map[rune]int
+	runeAry   []rune
+}
+
+type ByRune []rune
+
+func (r ByRune) Len() int           { return len(r) }
+func (r ByRune) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r ByRune) Less(i, j int) bool { return r[i] < r[j] }
+
+type ByRuneCount RuneMap
+
+func (r ByRuneCount) Len() int           { return len(r.runeAry) }
+func (r ByRuneCount) Swap(i, j int)      { r.runeAry[i], r.runeAry[j] = r.runeAry[j], r.runeAry[i] }
+func (r ByRuneCount) Less(i, j int) bool { return r.runeCount[r.runeAry[i]] < r.runeCount[r.runeAry[j]] }
 
 func Checksum(room string) string {
 	strs := strings.Split(room, "[")
@@ -28,4 +46,14 @@ func MergeMaps(maps []map[rune]int) map[rune]int {
 		}
 	}
 	return results
+}
+
+func CalcCheckSum(runeMap map[rune]int) []rune {
+	var keys []rune
+	for k := range runeMap {
+		keys = append(keys, k)
+	}
+	sort.Sort(ByRune(keys))
+
+	return keys
 }
